276b118621f60020aca87f97e2ceb71767343e69
saltukozelgul <saltukozelgul@gmail.com>
2023-04-21 15:31:52 +0300

    why is different from localll

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // import bootstrap css
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
-import { BrowserRouter, Switch, Route, Router } from "react-router-dom";
+import { Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import "./index.css";
 import { CategoryPage } from "./pages/CategoryPage";
@@ -11,11 +11,11 @@ function App() {
   return (
     <Navbar
       children={
-        <Switch>
+        <>
           <Route exact path="/" component={HomePage}></Route>
           <Route path="/category/:category" component={CategoryPage}></Route>
           <Route path="/movie/:id" component={DetailedMoviePage}></Route>
-        </Switch>
+        </>
       }
     />
   );
